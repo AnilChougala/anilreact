d8db09b22997f8d80e1fee25d6c6a3d437f3616b
AnilChougala <50862340+AnilChougala@users.noreply.github.com>
2019-07-30 10:01:05 +0530

    Initial commit

---
+++ src/actions/session.js
@@ -0,0 +1,28 @@
+import { users } from "../data/users";
+export function loginVitkar(credentials){
+    console.log(credentials);
+    const matchingUsers = users.filter(user => user.username == credentials.username);
+    const matchedUser = matchingUsers[0];
+    if(matchedUser == null){
+        return {
+            type: "LOGIN_FAILED",
+            payload : {}
+        }
+    }
+    if(matchedUser.password == credentials.password){
+        console.log("matched")
+        return {
+            type: "LOGIN",
+            payload : matchedUser
+        }
+    } else {
+        return {
+            type: "LOGIN_FAILED",
+            payload : {}
+        }
+    }
+    
+}
+export function logout(){
+
+}
